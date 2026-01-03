1355696663fd5b5c06a8afe6c194b92c51fed170
Shivansh-Pandey-4 <shivanshofficial8750@gmail.com>
2026-01-03 13:25:28 +0530

    feat: add see all notes link to visit /notes page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import NotesInput from "@/components/NotesInput";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="p-5 container mx-auto">
-      <h1 className="text-2xl font-bold">Notes App</h1>
+      <header className="flex justify-between">
+        <h1 className="text-2xl font-bold">Notes App</h1>
+        <nav>
+          <ul>
+            <Link href={"/notes"}>
+              <li className="text-lg font-serif underline hover:text-green-700 cursor-pointer">See All Notes</li>
+            </Link>
+          </ul>
+        </nav>
+      </header>
       <NotesInput />
     </div>
   );
