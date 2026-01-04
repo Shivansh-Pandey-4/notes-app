1d41d574e835bf1b686200b647013d2caa7ed0f9
Shivansh-Pandey-4 <shivanshofficial8750@gmail.com>
2026-01-04 13:24:55 +0530

    feat: add edit note & delete note

--- app/notes/_components/Card.tsx
+++ app/notes/_components/Card.tsx
@@ -1,27 +1,65 @@
 import { ICard } from "@/lib/types"
 
+interface ICardProps {
+    note: ICard;
+    deleteBtn: (id: string | number) => void;
+    deleteBtnId: string | number | null;
+    startEdit: (note: ICard) => void;
+    editBtnId: string | number | null;
+    editTitle: string;
+    editContent: string;
+    setEditTitle: React.Dispatch<React.SetStateAction<string>>;
+    setEditContent: React.Dispatch<React.SetStateAction<string>>;
+    handleUpdate: (id: string | number) => void;
+    updating: boolean;
+    cancelEdit: (note: ICard) => void;
+}
 
-export default function Card({ note, deleteBtn, deleteBtnId }: { note: ICard; deleteBtn: (id: string | number) => void; deleteBtnId: string | number | null; }) {
 
+
+export default function Card(props: ICardProps) {
+
+    const { deleteBtn, deleteBtnId, editBtnId, editContent, editTitle, note, setEditContent, setEditTitle, startEdit, handleUpdate, updating, cancelEdit } = props;
 
     return (
         <>
-            <section>
-                <h1 className="text-xl font-semibold mt-2">Title - {note.title}</h1>
-                <p className="mt-1 text-lg">Content - {note.content}</p>
-                <div className="flex gap-5 mt-4">
-                    <p className="text-gray-600">CreatedAt - {note.createdAt?.toString().split("T")[0]}</p>
-                    <p className="text-gray-600">UpdatedAt - {note.updatedAt?.toString().split("T")[0]}</p>
-                </div>
-            </section>
-            <section>
-                <div className="flex gap-3">
-                    <button className="border px-3 py-1 cursor-pointer hover:bg-green-400 rounded-md bg-green-300">Edit</button>
+            {
+                note.id === editBtnId ?
+                    (
+                        <div className="flex flex-col w-full">
 
-                    <button disabled={deleteBtnId === note.id ? true : false} onClick={() => deleteBtn(note.id)} className="border px-3 py-1 cursor-pointer hover:bg-red-600 bg-red-400 rounded-md disabled:opacity-50 disabled:cursor-not-allowed">{deleteBtnId === note.id ? "Deleting..." : "Delete"}</button>
+                            <input autoFocus required className="border border-gray-500 rounded-md p-2 my-3 focus:outline-none text-gray-700 focus:ring-1 ring-blue-500" type="text" placeholder="Enter Title" value={editTitle} onChange={(e) => setEditTitle(e.target.value)} />
 
-                </div>
-            </section>
+                            <textarea required placeholder="Enter Content ..." className="border border-gray-500 rounded-md mt-3 focus:outline-none text-gray-700 p-3 focus:ring-1 ring-blue-500" rows={5} value={editContent} onChange={(e) => setEditContent(e.target.value)} ></textarea>
+                            <div>
+                                <button disabled={updating} onClick={() => handleUpdate(note.id)} className="border border-white bg-blue-500 cursor-pointer px-3 py-1 rounded-md hover:bg-blue-600 mt-5 text-white disabled:opacity-50 disabled:cursor-not-allowed">{updating ? "Saving" : "Save"} </button>
+
+                                <button onClick={() => cancelEdit(note)} disabled={updating} className="ml-5 border bg-red-500 cursor-pointer px-2 py-1 rounded-md hover:bg-red-600 mt-5 disabled:opacity-50 disabled:cursor-not-allowed"> Cancel </button>
+                            </div>
+                        </div>
+                    ) :
+                    (
+                        <>
+                            <section>
+                                <h1 className="text-xl font-semibold mt-2">Title - {note.title}</h1>
+                                <p className="mt-1 text-lg">Content - {note.content}</p>
+                                <div className="flex gap-5 mt-4">
+                                    <p className="text-gray-600">CreatedAt - {note.createdAt?.toString().split("T")[0]}</p>
+                                    <p className="text-gray-600">UpdatedAt - {note.updatedAt?.toString().split("T")[0]}</p>
+                                </div>
+                            </section>
+                            <section>
+                                <div className="flex gap-3">
+                                    <button onClick={() => startEdit(note)} className="border px-3 py-1 cursor-pointer hover:bg-green-400 rounded-md bg-green-300">Edit</button>
+
+                                    <button disabled={deleteBtnId === note.id ? true : false} onClick={() => deleteBtn(note.id)} className="border px-3 py-1 cursor-pointer hover:bg-red-600 bg-red-400 rounded-md disabled:opacity-50 disabled:cursor-not-allowed">{deleteBtnId === note.id ? "Deleting..." : "Delete"}</button>
+
+                                </div>
+                            </section>
+                        </>
+                    )
+            }
+
         </>
     )
 }
--- app/notes/_components/NotesBody.tsx
+++ app/notes/_components/NotesBody.tsx
@@ -1,14 +1,20 @@
 "use client"
 
-import { ICard, INOTES } from "@/lib/types";
+import { ICard } from "@/lib/types";
 import Link from "next/link";
 import Card from "./Card";
 import { useEffect, useState } from "react";
 
+
 export default function NotesBody({ value }: { value: ICard[] }) {
 
     const [state, setState] = useState<ICard[]>([]);
+    const [editBtnId, setEditBtnId] = useState<string | number | null>(null);
     const [deleteBtnId, setDeleteBtnId] = useState<string | number | null>(null);
+
+    const [editTitle, setEditTitle] = useState("");
+    const [editContent, setEditContent] = useState("");
+    const [updating, setUpdating] = useState(false);
 
     async function deleteBtn(id: string | number) {
         setDeleteBtnId(id);
@@ -36,6 +42,59 @@ export default function NotesBody({ value }: { value: ICard[] }) {
             setDeleteBtnId(null);
         }
     }
+
+    function startEdit(note: ICard) {
+        setEditBtnId(note.id);
+        setEditTitle(note.title);
+        setEditContent(note.content);
+    }
+
+    function cancelEdit(note: ICard) {
+        setEditBtnId(null);
+        setEditTitle('');
+        setEditContent('');
+    }
+
+    async function handleUpdate(id: string | number) {
+
+        if (!editContent || !editTitle) {
+            alert("plz provide title and content");
+            return;
+        }
+        setUpdating(true);
+        try {
+            const response = await fetch(`http://localhost:3000/api/notes/${id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ title: editTitle, content: editContent })
+            })
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                alert(data.msg || "failed to update note")
+                return;
+            }
+
+            setState(state.map(note => note.id === id ? data.updatedNote : note));
+
+            alert(data.msg);
+            return;
+
+        } catch (error) {
+            alert(error || "something went down");
+            return;
+
+        } finally {
+            setEditBtnId(null);
+            setEditContent('');
+            setEditTitle('');
+            setUpdating(false);
+        }
+    }
+
 
     useEffect(() => {
         setState([...value]);
@@ -68,7 +127,7 @@ export default function NotesBody({ value }: { value: ICard[] }) {
                                 key={note.id}
                                 className="border border-gray-200 bg-gray-200 shadow-md p-5 my-5 rounded-lg flex justify-between items-center"
                             >
-                                <Card note={note} deleteBtn={deleteBtn} deleteBtnId={deleteBtnId} />
+                                <Card cancelEdit={cancelEdit} updating={updating} handleUpdate={handleUpdate} editTitle={editTitle} setEditTitle={setEditTitle} setEditContent={setEditContent} editContent={editContent} editBtnId={editBtnId} note={note} deleteBtn={deleteBtn} deleteBtnId={deleteBtnId} startEdit={startEdit} />
                             </div>
                         ))
                     )
